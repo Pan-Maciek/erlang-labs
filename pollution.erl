ffd621df1a3a39e28fccb4424c5572c81ab250cb
pan-maciek <kozieja.maciej@gmail.com>
2020-04-02 01:52:26 +0200

    error handling with macro

--- pollution.erl
+++ pollution.erl
@@ -1,9 +1,10 @@
 -module(pollution).
 -author(maciek).
 
+-define(Err, Error = {error, _} -> Error).
 -record(monitor, {names = #{}, state = #{}}).
 
--export([ createMonitor/0, addStation/3, addValue/5, removeValue/4, getOneValue/4 ]).
+-export([ createMonitor/0, addStation/3, addValue/5, removeValue/4, getOneValue/4, getStationMean/3 ]).
 
 createMonitor() -> #monitor{}.
 
@@ -16,49 +17,50 @@ addStation(Name, Coordinates, Monitor = #monitor{names = Names, state = State}) 
     {_, {ok, _}} -> {error, coordinates}
   end.
 
+getMeasurements(Name, Monitor = #monitor{ names = Names }) when is_list(Name) ->
+  case maps:find(Name, Names) of
+    error -> {error, name};
+    {ok, Coordinates} -> getMeasurements(Coordinates, Monitor)
+  end;
+getMeasurements(Coordinates, #monitor{ state = State }) when is_tuple(Coordinates) ->
+  case maps:find(Coordinates, State) of
+    error -> {error, coordinates};
+    {ok, Measurements} -> {Coordinates, Measurements}
+  end.
+
 addMeasurement(Date, Type, Value, Measurements) ->
-  case lists:any(fun ({XDate, XType, _}) -> (XType == Type) andalso (XDate == Date) end, Measurements) of 
-    true -> error;
-    _ -> [{ {Date, Type}, Value } | Measurements]
+  case lists:keyfind({Date, Type}, 1, Measurements) of 
+    false -> [{ {Date, Type}, Value } | Measurements];
+    _ -> {error, measurement}
   end.
 
-addValue(Name, Date, Type, Value, Monitor = #monitor{names = Names}) when is_list(Name) -> 
-  case maps:find(Name, Names) of
-    error -> {error, name};
-    {ok, Coordinates} -> addValue(Coordinates, Date, Type, Value, Monitor)
-  end;
-addValue(Coordinates, Date, Type, Value, Monitor = #monitor{state = State}) when is_tuple(Coordinates) -> 
-  case maps:find(Coordinates, State) of
-    {ok, Measurements} -> 
-      case addMeasurement(Date, Type, Value, Measurements) of
-        error -> {error, measurement};
+addValue(Id, Date, Type, Value, Monitor = #monitor{state = State}) -> 
+  case getMeasurements(Id, Monitor) of ?Err;
+    {Coordinates, Measurements} ->
+      case addMeasurement(Date, Type, Value, Measurements) of ?Err;
         ExtMeasurements -> Monitor#monitor{state = State#{ Coordinates => ExtMeasurements }}
-      end;
-    _ -> {error, coordinates}
+      end
   end.
 
-removeValue(Name, Date, Type, Monitor = #monitor{names = Names}) when is_list(Name) ->
-  case maps:find(Name, Names) of
-    error -> {error, name};
-    {ok, Coordinates} -> removeValue(Coordinates, Date, Type, Monitor)
-  end;
-removeValue(Coordinates, Date, Type, Monitor = #monitor{state = State}) when is_tuple(Coordinates) ->
-  case maps:find(Coordinates, State) of
-    error -> {error, coordinates};
-    {ok, Measurements} -> Monitor#monitor{state = State#{ Coordinates => lists:keydelete({ Date, Type }, 1, Measurements) }}
+removeValue(Id, Date, Type, Monitor = #monitor{state = State}) ->
+  case getMeasurements(Id, Monitor) of ?Err;
+    {Coordinates, Measurements} -> Monitor#monitor{state = State#{ Coordinates => lists:keydelete({ Date, Type }, 1, Measurements) }}
   end.
 
-getOneValue(Name, Date, Type, Monitor = #monitor{names = Names}) when is_list(Name) ->
-  case maps:find(Name, Names) of
-    error -> {error, name};
-    {ok, Coordinates} -> getOneValue(Coordinates, Date, Type, Monitor)
-  end;
-getOneValue(Coordinates, Date, Type, #monitor{state = State}) when is_tuple(Coordinates) ->
-  case maps:find(Coordinates, State) of
-    error -> {error, coordinates};
-    {ok, Measurements} ->
+getOneValue(Id, Date, Type, Monitor = #monitor{}) ->
+  case getMeasurements(Id, Monitor) of ?Err;
+    {_, Measurements} ->
       case lists:keytake({ Date, Type }, 1, Measurements) of
         {value, {_, Value}, _} -> Value;
         _ -> {error, not_found}
       end
   end.
+
+mean(List) -> lists:sum(List) / length(List).
+
+getStationMean(Coordinates, Type, #monitor{state = State}) when is_tuple(Coordinates) ->
+  case maps:find(Coordinates, State) of
+    error -> {error, coordinates};
+    {ok, Measurements} -> mean([Value || {_, XType, Value} <- Measurements, XType == Type])
+  end.
+
